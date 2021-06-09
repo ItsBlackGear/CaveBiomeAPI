1e900736ecbed0aeab8542b1b6ad3b4312f5fd14
Alex Sarnese <40846040+TelepathicGrunt@users.noreply.github.com>
2021-06-09 14:54:54 -0400

    UNDO

--- src/main/java/com/blackgear/cavebiomes/mixin/ChunkGeneratorMixin.java
+++ src/main/java/com/blackgear/cavebiomes/mixin/ChunkGeneratorMixin.java
@@ -1,5 +1,6 @@
 package com.blackgear.cavebiomes.mixin;
 
+import com.blackgear.cavebiomes.common.util.BiomeCoordinates;
 import com.blackgear.cavebiomes.core.api.CaveLayer;
 import net.minecraft.crash.CrashReport;
 import net.minecraft.crash.ReportedException;
@@ -12,9 +13,11 @@ import net.minecraft.world.gen.WorldGenRegion;
 import net.minecraft.world.gen.feature.structure.StructureManager;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 //<>
@@ -47,7 +50,12 @@ public class ChunkGeneratorMixin {
 //            throw new ReportedException(crashReport);
 //        }
 //    }
-    
+
+    @Redirect(method = "func_230351_a_", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/biome/provider/BiomeProvider;getNoiseBiome(III)Lnet/minecraft/world/biome/Biome;"))
+    private Biome getSurfaceNoiseBiome(BiomeProvider biomeProvider, int x, int y, int z) {
+        return biomeProvider.getNoiseBiome(x, 64, z);
+    }
+
     @Inject(method = "func_230351_a_", at = @At("RETURN"), cancellable = true)
     private void getCaveNoiseBiome(WorldGenRegion region, StructureManager manager, CallbackInfo ci) {
         int mainChunkX = region.getMainChunkX();
