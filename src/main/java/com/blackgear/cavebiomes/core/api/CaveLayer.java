bc40a42628f51e7fbaba820bb596eeb55666e49a
BlackGear27 <itsblackgear@gmail.com>
2021-06-13 03:02:30 -0400

    1.16.3-1.16.4 compat and layer merge

--- src/main/java/com/blackgear/cavebiomes/core/api/CaveLayer.java
+++ src/main/java/com/blackgear/cavebiomes/core/api/CaveLayer.java
@@ -4,11 +4,13 @@ import net.minecraft.util.RegistryKey;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.IExtendedNoiseRandom;
+import net.minecraft.world.gen.INoiseRandom;
 import net.minecraft.world.gen.LazyAreaLayerContext;
 import net.minecraft.world.gen.area.IAreaFactory;
 import net.minecraft.world.gen.area.LazyArea;
 import net.minecraft.world.gen.layer.Layer;
 import net.minecraft.world.gen.layer.ZoomLayer;
+import net.minecraft.world.gen.layer.traits.IAreaTransformer0;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -33,7 +35,7 @@ public class CaveLayer {
 
         LongFunction<IExtendedNoiseRandom<LazyArea>> context = salt -> new LazyAreaLayerContext(25, seed, salt);
 
-        IAreaFactory<LazyArea> factory = new CaveMasterLayer(biomeRegistry, caveBiomes).apply(context.apply(200L));
+        IAreaFactory<LazyArea> factory = new CaveBiomeLayer(biomeRegistry, caveBiomes).apply(context.apply(200L));
 
         for (int size = 0; size < biomeSize; size++) {
             if ((size + 2) % 3 != 0) {
@@ -44,5 +46,20 @@ public class CaveLayer {
         }
 
         return new Layer(factory);
+    }
+
+    public static class CaveBiomeLayer implements IAreaTransformer0 {
+        public final Registry<Biome> dynamicBiomeRegistry;
+        private final List<Biome> biomes;
+
+        public CaveBiomeLayer(Registry<Biome> biomeRegistry, List<Biome> biomes) {
+            this.dynamicBiomeRegistry = biomeRegistry;
+            this.biomes = biomes;
+        }
+
+        @Override
+        public int apply(INoiseRandom random, int x, int z) {
+            return this.dynamicBiomeRegistry.getId(this.biomes.get(random.random(this.biomes.size())));
+        }
     }
 }
--- src/main/java/com/blackgear/cavebiomes/core/registries/CaveBiomes.java
+++ src/main/java/com/blackgear/cavebiomes/core/registries/CaveBiomes.java
@@ -30,7 +30,7 @@ public class CaveBiomes {
     public static Biome makeDefaultCaves() {
         MobSpawnInfo.Builder spawnSettings = new MobSpawnInfo.Builder();
         DefaultBiomeFeatures.withBatsAndHostiles(spawnSettings);
-        BiomeGenerationSettings.Builder generationSettings = new BiomeGenerationSettings.Builder().withSurfaceBuilder(ConfiguredSurfaceBuilders.GRASS);
+        BiomeGenerationSettings.Builder generationSettings = new BiomeGenerationSettings.Builder().withSurfaceBuilder(ConfiguredSurfaceBuilders.field_244178_j);
         DefaultBiomeFeatures.withStrongholdAndMineshaft(generationSettings);
         generationSettings.withStructure(StructureFeatures.RUINED_PORTAL);
         DefaultBiomeFeatures.withCavesAndCanyons(generationSettings);
@@ -42,7 +42,7 @@ public class CaveBiomes {
         DefaultBiomeFeatures.withSugarCaneAndPumpkins(generationSettings);
         DefaultBiomeFeatures.withLavaAndWaterSprings(generationSettings);
         DefaultBiomeFeatures.withFrozenTopLayer(generationSettings);
-        return new Biome.Builder().precipitation(Biome.RainType.RAIN).category(Biome.Category.NONE).depth(0.125F).scale(0.05F).temperature(0.8F).downfall(0.4F).setEffects(new BiomeAmbience.Builder().setWaterColor(4159204).setWaterFogColor(329011).setFogColor(12638463).withSkyColor(getSkyColorWithTemperatureModifier()).setMoodSound(MoodSoundAmbience.DEFAULT_CAVE).build()).withMobSpawnSettings(spawnSettings.build()).withGenerationSettings(generationSettings.build()).build();
+        return new Biome.Builder().precipitation(Biome.RainType.RAIN).category(Biome.Category.NONE).depth(0.125F).scale(0.05F).temperature(0.8F).downfall(0.4F).setEffects(new BiomeAmbience.Builder().setWaterColor(4159204).setWaterFogColor(329011).setFogColor(12638463).withSkyColor(getSkyColorWithTemperatureModifier()).setMoodSound(MoodSoundAmbience.DEFAULT_CAVE).build()).withMobSpawnSettings(spawnSettings.copy()).withGenerationSettings(generationSettings.build()).build();
     }
 
     private static int getSkyColorWithTemperatureModifier() {
